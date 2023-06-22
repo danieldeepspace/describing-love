6e7f821087cc7c4da8cc452c0fbb065b97885542
Frank Lujan <franklujan@Franks-MBP.attlocal.net>
2023-06-22 11:22:13 -0500

    add window listener script

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,22 @@ if (iframe) {
 	iframe.src = 'https://oh.connectionofcommunity.com/oh-form/cn/'+queryString;
 	// iframe.src = 'http://localhost:3000/oh-form/cn'+queryString;
 }
+// Use site url as form submission origin
+window.addEventListener('message', (event) => {
+	console.log(event.origin);
+	// Check the origin of the message for security purposes
+	const regex = /^https?:\/\/(oh.connectionofcommunity.com)/i;
+	if (!regex.test(event.origin)) {
+	  return;
+	}
+  
+	// Process the received message
+	if (event.data === 'requestParentURL') {
+		console.log('sending origin to Seeker Portal!')
+	  // Send the parent URL back to the iframe
+	  event.source.postMessage(
+		{ type: 'parentURL', url: window.location.href },
+		event.origin
+	  );
+	}
+});
